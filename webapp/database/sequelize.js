b584e4b020b43e1e6bcd21e77a89c8ebcf06fbed
DeepGamit <deepkgamit@gmail.com>
2020-10-04 17:57:53 -0400

    Relationships between Models added

--- webapp/database/sequelize.js
+++ webapp/database/sequelize.js
@@ -16,13 +16,22 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
       acquire: dbConfig.pool.acquire,
       idle: dbConfig.pool.idle
     }
-  });
+});
   
-  const db = {};
+const db = {};
   
-  db.Sequelize = Sequelize;
-  db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
 
-  db.user = require("../models/User")(sequelize, Sequelize);
+db.user = require("../models/User")(sequelize, Sequelize);
+db.question = require('../models/Question')(sequelize, Sequelize);
+db.category = require('../models/Category')(sequelize,Sequelize);
+db.answer = require('../models/Answer')(sequelize,Sequelize);
 
-  module.exports = db;
+db.user.hasMany(db.question, {as: 'questions', foreignKey: 'user_id'});
+db.question.hasMany(db.answer, {as: 'answers', foreignKey:'question_id'});
+db.user.hasMany(db.answer, {as: 'answers', foreignKey:'user_id'});
+db.question.belongsToMany(db.category, { as: 'categories', through: 'question_category', foreignKey: 'question_id', otherKey: 'category_id' });
+db.category.belongsToMany(db.question, { as:'questions', through: 'question_category', foreignKey: 'category_id', otherKey: 'question_id' });
+
+module.exports = db;
