8ea9c50da43d55d903c6862820c57a8330b4dcfc
DeepGamit <deepkgamit@gmail.com>
2020-11-18 00:45:45 -0500

    code-deploy script changes

--- webapp/server.js
+++ webapp/server.js
@@ -1,14 +1,18 @@
 const app = require('./app');
+const logger = require('../config/logger');
+
 
 
 // Database Connection
 const db = require("./database/sequelize");
 db.sequelize.sync({ force: false }).then(() => {
     console.log(`Database Connected.`);
+    logger.info("Database Connected...");
 });
 
 // Set port, Listen for Requests
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
+  logger.info("Server Started...");
 });
